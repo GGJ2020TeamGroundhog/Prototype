a72ce4c232004c05e210dda11d54fe5bf2843d62
StoneKnight117 <60109362+StoneKnight117@users.noreply.github.com>
2020-02-01 17:49:42 -0500

    Add files via upload

---
+++ NPC.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : Interactables
+{
+    public DialogueTrigger dialogueTrigger;
+    public Dialogue dialogue;
+
+    private void Start() {
+        dialogueTrigger.dialogue = dialogue;
+    }
+
+    public override void Interact(string noUse) {
+        dialogueTrigger.StartDialogue();
+    }
+}
